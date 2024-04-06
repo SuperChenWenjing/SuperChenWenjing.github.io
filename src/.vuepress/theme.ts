e91747f3d3bb4b2106a8de98322a03ca42da30d3
陈文景 <2646530197@qq.com>
2024-04-06 12:01:36 +0800

    Init Project

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -21,7 +21,7 @@ export default hopeTheme({
   iconAssets: [
     "//at.alicdn.com/t/c/font_3538491_of8auydfb4f.css", // 图标01
     "//at.alicdn.com/t/c/font_4451850_mrbcn3myxyd.css", // 图标02
-    "//at.alicdn.com/t/c/font_4495756_nbp9rer1cu.css", // 图标03
+    "//at.alicdn.com/t/c/font_4495756_agzcqua6lhj.css", // 图标03
   ],
 
   logo: "/logo.svg", // 导航栏图标
