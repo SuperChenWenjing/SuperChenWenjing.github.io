689c3725c5896f6c160dc9a722a8f32dde833a34
SuperChenWenjing <70443685+SuperChenWenjing@users.noreply.github.com>
2024-05-05 06:43:07 +0000

    Deploying to gh-pages from @ SuperChenWenjing/SuperChenWenjing.github.io@18517585d1c1a729dd08f1c1bed48918b874c713 🚀

---
+++ assets/sort.html-B-j84Lit.js
@@ -0,0 +1,24 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as a,f as o}from"./app-BHY_jafT.js";const n={},s=o(`<h1 id="sort" tabindex="-1"><a class="header-anchor" href="#sort"><span>sort</span></a></h1><p><code>sort</code> 命令用于文本文件的排序。</p><p>下面是 <code>example.txt</code> 文件的内容。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code>f
+b
+c
+g
+a
+e
+d
+</code></pre></div><p>执行 <code>sort</code> 命令对其进行排序。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code>$ <span class="token function">sort</span> example.txt
+a
+b
+c
+d
+e
+f
+g
+</code></pre></div><h2 id="参数" tabindex="-1"><a class="header-anchor" href="#参数"><span>参数</span></a></h2><p><code>-R</code> 参数表示随机排序。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">sort</span> <span class="token parameter variable">-R</span> example.txt
+b
+d
+a
+c
+g
+e
+f
+</code></pre></div>`,9),r=[s];function c(i,p){return t(),a("div",null,r)}const h=e(n,[["render",c],["__file","sort.html.vue"]]),m=JSON.parse('{"path":"/casual/linux/bash/archives/commands/sort.html","title":"sort","lang":"zh-CN","frontmatter":{"date":"2024-03-02T00:00:00.000Z","description":"sort sort 命令用于文本文件的排序。 下面是 example.txt 文件的内容。 执行 sort 命令对其进行排序。 参数 -R 参数表示随机排序。 ","head":[["meta",{"property":"og:url","content":"https://SuperChenWenjing.github.io/casual/linux/bash/archives/commands/sort.html"}],["meta",{"property":"og:site_name","content":"陈文景"}],["meta",{"property":"og:title","content":"sort"}],["meta",{"property":"og:description","content":"sort sort 命令用于文本文件的排序。 下面是 example.txt 文件的内容。 执行 sort 命令对其进行排序。 参数 -R 参数表示随机排序。 "}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-03-08T16:03:07.000Z"}],["meta",{"property":"article:author","content":"陈文景"}],["meta",{"property":"article:published_time","content":"2024-03-02T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-03-08T16:03:07.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"sort\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2024-03-02T00:00:00.000Z\\",\\"dateModified\\":\\"2024-03-08T16:03:07.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"陈文景\\",\\"email\\":\\"2646530197@qq.com\\",\\"url\\":\\"https://superchenwenjing.github.io\\"}]}"]]},"headers":[{"level":2,"title":"参数","slug":"参数","link":"#参数","children":[]}],"git":{"createdTime":1709913787000,"updatedTime":1709913787000,"contributors":[{"name":"陈文景","email":"2646530197@qq.com","commits":1}]},"readingTime":{"minutes":0.26,"words":77},"filePathRelative":"casual/linux/bash/archives/commands/sort.md","localizedDate":"2024年3月2日","excerpt":"","autoDesc":true}');export{h as comp,m as data};
